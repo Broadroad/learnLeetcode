a8895b7814092ce71c693291ac4019480634f6e4
broadroad <tjbroadroad@163.com>
2019-09-01 14:26:46 +0800

    backstrace accept

--- 22.generate-parentheses.cpp
+++ 22.generate-parentheses.cpp
@@ -1,8 +1,8 @@
 #include <stack> 
 #include <vector>
 #include <set>
-#include <iostream>
-using namespace std;
+// #include <iostream>
+// using namespace std;
 /*
  * @lc app=leetcode id=22 lang=cpp
  *
@@ -38,73 +38,122 @@ using namespace std;
  */
 class Solution {
 public:
-    vector<string> generateParenthesis(int n) {
-        vector<string> ret;
-        if (n < 1) {
-            return ret;
-        }
+    // vector<string> generateParenthesis1(int n) {
+    //     vector<string> ret;
+    //     if (n < 1) {
+    //         return ret;
+    //     }
 
-        string str = "";
-        for (int i = 0; i < n; i++) {
-            str += "()";
-        }
+    //     string str = "";
+    //     for (int i = 0; i < n; i++) {
+    //         str += "()";
+    //     }
 
-        string path = "";
-        set<string> result;
-        DFS(str, 0, path, result);
-        ret.resize(result.size());
-        copy(result.begin(), result.end(), ret.begin());
-        return ret;
-    }
+    //     string path = "";
+    //     set<string> result;
+    //     DFS1(str, 0, path, result);
+    //     ret.resize(result.size());
+    //     copy(result.begin(), result.end(), ret.begin());
+    //     return ret;
+    // }
 
-    bool isValid(string str) {
-        stack<char> brackets;
+    // bool isValid(string str) {
+    //     stack<char> brackets;
     
-        for (char c : str) {
-            if (c == '(') {
-                brackets.push(c);
-            } else {
-                if (!brackets.empty()) {
-                    char ch = brackets.top();
-                    brackets.pop();
-                    if (ch == ')') {
-                        return false;
-                    }
-                    continue;
-                }
-                return false;
-            }
-        }
+    //     for (char c : str) {
+    //         if (c == '(') {
+    //             brackets.push(c);
+    //         } else {
+    //             if (!brackets.empty()) {
+    //                 char ch = brackets.top();
+    //                 brackets.pop();
+    //                 if (ch == ')') {
+    //                     return false;
+    //                 }
+    //                 continue;
+    //             }
+    //             return false;
+    //         }
+    //     }
         
-        if (brackets.empty()) {
-            return true;
-        }
-        return false;
+    //     if (brackets.empty()) {
+    //         return true;
+    //     }
+    //     return false;
+    // }
+
+    // void DFS1(string s, int pos, string path, set<string> &result) {
+    //     if (pos == s.size()) {
+            
+    //         if (isValid(path)) {
+    //             result.insert(path);
+    //         }
+    //         return;
+    //     } 
+
+    //     for (int i = pos; i < s.size(); i++) {
+    //         path.push_back(s[i]);
+    //         DFS1(s, pos+1, path, result);
+    //         path.pop_back();
+    //     };        
+    // }
+
+    //  vector<string> generateParenthesis2(int n) {
+    //     vector<string> ret;
+    //     if (n < 1) {
+    //         return ret;
+    //     }
+
+    //     string str = "";
+    //     for (int i = 0; i < n; i++) {
+    //         str += "()";
+    //     }
+    //     sort(str.begin(), str.end());
+
+    //     set<string> result;
+    //     DFS2(str, 0, result);
+    //     ret.resize(result.size());
+    //     copy(result.begin(), result.end(), ret.begin());
+    //     return ret;
+    //  }
+
+    //  void DFS2(string s, int pos, set<string> &result) {
+    //     if (pos == s.size() - 1) {
+    //         if (isValid(s)) {
+    //             result.insert(s);
+    //         }
+    //         return;
+    //     } 
+
+    //    for (int i = pos; i < s.size(); i++) {
+    //        if (i != pos && s[i] == s[pos]) {
+    //            continue;
+    //        }
+    //        swap(s[i], s[pos]);
+    //        DFS2(s, pos+1, result);
+    //        swap(s[i], s[pos]);
+    //    }       
+    // }
+
+    vector<string> generateParenthesis(int n) {
+        vector<string> res;
+        DFS3("", n, n, res);
+        return res;
     }
 
-    void DFS(string s, int pos, string path, set<string> &result) {
-        if (pos == s.size()) {
-            
-            if (isValid(path)) {
-                result.insert(path);
-            }
-            return;
-        } 
-
-        for (int i = pos; i < s.size(); i++) {
-            path.push_back(s[i]);
-            DFS(s, pos+1, path, result);
-            path.pop_back();
-        };        
+    void DFS3(string s, int opens, int closes, vector<string> &res) {
+        if (!opens && !closes) res.push_back(s);
+        if (opens) DFS3(s + '(', opens - 1, closes, res);
+        if (opens < closes) DFS3(s + ')', opens, closes-1, res);
     }
 };
 
 
-int main() {
-    Solution s;
-    vector<string> r = s.generateParenthesis(5);
-    for (auto str : r) {
-        cout << str << endl;
-    }
-}
+// int main() {
+//     Solution s;
+//     vector<string> r = s.generateParenthesis1(5);
+//     for (auto str : r) {
+//         cout << str << endl;
+//     }
+// }
 
