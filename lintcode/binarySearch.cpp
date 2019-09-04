02096694ba2c314821c2943bc1256e57d8123f06
broadroad <tjbroadroad@163.com>
2019-09-04 21:47:17 +0800

    binarySearch

---
+++ lintcode/binarySearch.cpp
@@ -0,0 +1,46 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+class Solution {
+public:
+    /**
+     * @param nums: An integer array sorted in ascending order
+     * @param target: An integer
+     * @return: An integer
+     */
+    int findPosition(vector<int> &nums, int target) {
+        if (nums.size() == 0) {
+            return -1;
+        }
+        
+        int start = 0, end = nums.size() - 1;
+        while (start + 1 < end) {
+            int mid = start + (end - start) / 2;
+            if (nums[mid] == target) {
+                end = mid;
+            } else if (nums[mid] < target) {
+                start = mid;
+            } else {
+                end = mid;
+            }
+        }
+        
+        if (nums[start] == target) {
+            return start;
+        }
+        
+        if (nums[end] == target) {
+            return end;
+        }
+        return -1;
+    }
+};
+
+int main() {
+    Solution s;
+    vector<int> nums;
+    nums.push_back(1);
+    nums.push_back(1);
+    cout << s.findPosition(nums, 1) << endl;
+}
