fbc0d60f0da2983e2efff0fba3637848ff9ac877
broadroad <tjbroadroad@163.com>
2019-09-13 16:59:22 +0800

    update search2Dx in one while loop regart 2d as 1d array

--- lintcode/search2Dx.cpp
+++ lintcode/search2Dx.cpp
@@ -52,4 +52,27 @@ public:
         }
         return false;
     }
+
+    bool searchMatrix2(vector<vector<int>> &matrix, int target) {
+        if (matrix.size() == 0 || matrix[0].size() == 0) {
+            return false;
+        }
+
+        int row = matrix.size();
+        int col = matrix[0].size();
+
+        int start = 0, end = row * col - 1;
+        while (start <= end) {
+            int mid = start + (end - start) / 2;
+            int number = matrix[mid/col][mid%col];
+            if (number == target) {
+                return true;
+            } else if (number < target) {
+                start = mid + 1;
+            } else {
+                end = mid - 1;
+            }
+        }
+        return false;
+    }
 };
