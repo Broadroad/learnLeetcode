b8cf11f5b96569f5e1b31bbf90d9e08df08d5533
Kang <kang.zhang@emc.com>
2019-11-29 17:16:25 +0800

    138

--- 138.copy-list-with-random-pointer.cpp
+++ 138.copy-list-with-random-pointer.cpp
@@ -67,7 +67,68 @@ public:
 class Solution {
 public:
     Node* copyRandomList(Node* head) {
-        
+        /*
+        if (!head) {
+            return nullptr;
+        }
+
+        unordered_map<Node*, Node*> m;
+        Node* l1 = head;
+        Node* l2Head = new Node(0, nullptr, nullptr);
+        Node* prev = l2Head;
+
+        while (l1) {
+            Node* l2 = new Node();
+            m[l1] = l2;
+            l1 = l1->next;
+            l2->random = nullptr;
+            l2->next = nullptr;
+            prev->next = l2;
+            prev = l2;
+        }
+
+        l1 = head;
+        while (l1) {
+            Node* n = m[l1];
+            n->random = m[l1->random];
+            n->val = l1->val;
+            l1 = l1->next;
+        }
+        return l2Head->next;
+        */
+       if (!head) {
+           return nullptr;
+       }
+
+       Node* l1 = head;
+       while (l1) {
+           Node* next = l1->next;
+           Node* l11 = new Node(l1->val, next, nullptr);
+           l1->next = l11;
+           l1 = next;
+       }
+
+       l1 = head;
+       Node* l2 = l1->next;
+       Node* ans = l2;
+       while (l1) {
+           l2 = l1->next;
+           if (l1->random) {
+                l2->random = l1->random->next;
+           }
+           l1 = l2->next;
+       }
+
+       l1 = head;
+       while (l1) {
+           l2 = l1->next;
+           l1->next = l2->next;
+           if (l2->next) {
+            l2->next = l2->next->next;
+           }
+           l1 = l1->next;
+       }
+       return ans;
     }
 };
 // @lc code=end
