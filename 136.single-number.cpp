620501ee24414c1e3cf3da5622c9a893585f40a1
Kang <kang.zhang@emc.com>
2020-01-02 14:12:43 +0800

    bit and single number

---
+++ 136.single-number.cpp
@@ -0,0 +1,52 @@
+/*
+ * @lc app=leetcode id=136 lang=cpp
+ *
+ * [136] Single Number
+ *
+ * https://leetcode.com/problems/single-number/description/
+ *
+ * algorithms
+ * Easy (62.33%)
+ * Likes:    3234
+ * Dislikes: 125
+ * Total Accepted:    590.6K
+ * Total Submissions: 947.1K
+ * Testcase Example:  '[2,2,1]'
+ *
+ * Given a non-empty array of integers, every element appears twice except for
+ * one. Find that single one.
+ * 
+ * Note:
+ * 
+ * Your algorithm should have a linear runtime complexity. Could you implement
+ * it without using extra memory?
+ * 
+ * Example 1:
+ * 
+ * 
+ * Input: [2,2,1]
+ * Output: 1
+ * 
+ * 
+ * Example 2:
+ * 
+ * 
+ * Input: [4,1,2,1,2]
+ * Output: 4
+ * 
+ * 
+ */
+
+// @lc code=start
+class Solution {
+public:
+    int singleNumber(vector<int>& nums) {
+        int ans = 0;
+        for (int num : nums) {
+            ans = ans ^ num;
+        }
+        return ans;
+    }
+};
+// @lc code=end
+
---
+++ 137.single-number-ii.cpp
@@ -0,0 +1,56 @@
+/*
+ * @lc app=leetcode id=137 lang=cpp
+ *
+ * [137] Single Number II
+ *
+ * https://leetcode.com/problems/single-number-ii/description/
+ *
+ * algorithms
+ * Medium (48.00%)
+ * Likes:    1122
+ * Dislikes: 295
+ * Total Accepted:    190.3K
+ * Total Submissions: 396.4K
+ * Testcase Example:  '[2,2,3,2]'
+ *
+ * Given a non-empty array of integers, every element appears three times
+ * except for one, which appears exactly once. Find that single one.
+ * 
+ * Note:
+ * 
+ * Your algorithm should have a linear runtime complexity. Could you implement
+ * it without using extra memory?
+ * 
+ * Example 1:
+ * 
+ * 
+ * Input: [2,2,3,2]
+ * Output: 3
+ * 
+ * 
+ * Example 2:
+ * 
+ * 
+ * Input: [0,1,0,1,0,1,99]
+ * Output: 99
+ * 
+ */
+
+// @lc code=start
+class Solution {
+public:
+    int singleNumber(vector<int>& nums) {
+        int ans = 0;
+        int x1 = 0, x2 = 0, mask = 0;
+        for (int num : nums) {
+            x2 ^= x1 & num;
+            x1 ^= num;
+            mask = ~(x1 & x2);
+            x2 &= mask;
+            x1 &= mask;
+        }
+        return x1;
+    }
+};
+// @lc code=end
+
